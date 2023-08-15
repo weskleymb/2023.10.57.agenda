2a91f9fad60ac184ecd5fa57a0aa1d8e9b4914c7
Weskley Bezerra <weskleymb@hotmail.com>
2023-08-15 20:59:01 -0300

    adicao da funcionalidade de edicao e remocao

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,7 @@
 import { Contato } from "./model/contato.js";
 
 let contatos: Contato[] = [];
+let indiceEdicao: number = null;
 
 document.addEventListener('DOMContentLoaded', () => {
     const formulario = document.getElementById('formulario-de-cadastro') as HTMLFormElement;
@@ -15,23 +16,74 @@ function submeterFormulario(evento: Event): void {
 
     const contato = new Contato(nome.value, fone.value);
 
-    contatos.push(contato);
+    if (indiceEdicao === null) {
+        contatos.push(contato);
+    } else {
+        contatos[indiceEdicao] = contato;
+        indiceEdicao = null;
+    }
 
     atualizarListaDeContatos(contatos);
 
+    limparFormulario(nome, fone);
+
+}
+
+function limparFormulario(nome: HTMLInputElement, 
+                          fone: HTMLInputElement): void {
     nome.value = '';
     fone.value = '';
     nome.focus();
-
-
 }
+
 
 function atualizarListaDeContatos(contatos: Contato[]): void {
     const listaDeContatos = document.querySelector('#lista-de-contatos tbody') as HTMLTableElement;
     listaDeContatos.innerHTML = '';
-    contatos.forEach(contato => {
+    contatos.forEach((contato, indice) => {
         const linhaDaTabela = listaDeContatos.insertRow();
         linhaDaTabela.insertCell().textContent = contato.nome;
         linhaDaTabela.insertCell().textContent = contato.fone;
+
+        const iconeEditar = document.createElement('i');
+        iconeEditar.className = 'fas fa-edit';
+        
+        const botaoEditar = document.createElement('button');
+        botaoEditar.className = 'btn btn-success btn-sm me-2';
+        botaoEditar.onclick = () => editarContato(indice);
+        botaoEditar.appendChild(iconeEditar);
+
+        const iconeExcluir = document.createElement('i');
+        iconeExcluir.className = 'fas fa-trash-alt';
+
+        const botaoExcluir = document.createElement('button');
+        botaoExcluir.className = 'btn btn-danger btn-sm';
+        botaoExcluir.onclick = () => excluirContato(indice);
+        botaoExcluir.appendChild(iconeExcluir);
+
+        linhaDaTabela.insertCell().append(botaoEditar, botaoExcluir);
     });
 }
+function editarContato(indice: number): void {
+    // implementacao
+    const contato = contatos[indice];
+    indiceEdicao = indice;
+
+    const nome = document.getElementById('nome') as HTMLInputElement;
+    const fone = document.getElementById('fone') as HTMLInputElement;
+
+    nome.value = contato.nome;
+    fone.value = contato.fone;
+
+}
+
+function excluirContato(indice: number): void {
+    // implementacao
+    const contato = contatos[indice];
+    const confirme = confirm(`Deseja excluir o contato ${contato.nome}?`);
+    if (confirme) {
+        contatos.splice(indice, 1);
+        atualizarListaDeContatos(contatos);
+    }
+}
+
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,6 @@
 import { Contato } from "./model/contato.js";
 let contatos = [];
+let indiceEdicao = null;
 document.addEventListener('DOMContentLoaded', () => {
     const formulario = document.getElementById('formulario-de-cadastro');
     formulario.onsubmit = submeterFormulario;
@@ -9,8 +10,17 @@ function submeterFormulario(evento) {
     const nome = document.getElementById('nome');
     const fone = document.getElementById('fone');
     const contato = new Contato(nome.value, fone.value);
-    contatos.push(contato);
+    if (indiceEdicao === null) {
+        contatos.push(contato);
+    }
+    else {
+        contatos[indiceEdicao] = contato;
+        indiceEdicao = null;
+    }
     atualizarListaDeContatos(contatos);
+    limparFormulario(nome, fone);
+}
+function limparFormulario(nome, fone) {
     nome.value = '';
     fone.value = '';
     nome.focus();
@@ -18,9 +28,40 @@ function submeterFormulario(evento) {
 function atualizarListaDeContatos(contatos) {
     const listaDeContatos = document.querySelector('#lista-de-contatos tbody');
     listaDeContatos.innerHTML = '';
-    contatos.forEach(contato => {
+    contatos.forEach((contato, indice) => {
         const linhaDaTabela = listaDeContatos.insertRow();
         linhaDaTabela.insertCell().textContent = contato.nome;
         linhaDaTabela.insertCell().textContent = contato.fone;
+        const iconeEditar = document.createElement('i');
+        iconeEditar.className = 'fas fa-edit';
+        const botaoEditar = document.createElement('button');
+        botaoEditar.className = 'btn btn-success btn-sm me-2';
+        botaoEditar.onclick = () => editarContato(indice);
+        botaoEditar.appendChild(iconeEditar);
+        const iconeExcluir = document.createElement('i');
+        iconeExcluir.className = 'fas fa-trash-alt';
+        const botaoExcluir = document.createElement('button');
+        botaoExcluir.className = 'btn btn-danger btn-sm';
+        botaoExcluir.onclick = () => excluirContato(indice);
+        botaoExcluir.appendChild(iconeExcluir);
+        linhaDaTabela.insertCell().append(botaoEditar, botaoExcluir);
     });
+}
+function editarContato(indice) {
+    // implementacao
+    const contato = contatos[indice];
+    indiceEdicao = indice;
+    const nome = document.getElementById('nome');
+    const fone = document.getElementById('fone');
+    nome.value = contato.nome;
+    fone.value = contato.fone;
+}
+function excluirContato(indice) {
+    // implementacao
+    const contato = contatos[indice];
+    const confirme = confirm(`Deseja excluir o contato ${contato.nome}?`);
+    if (confirme) {
+        contatos.splice(indice, 1);
+        atualizarListaDeContatos(contatos);
+    }
 }
